e299fff6dca06a33708255631400128faa222b7c
Francisco Saraiva <fnmss03@gmail.com>
2026-06-02 19:50:41 +0100

    Slight optimization on problem 168

--- 01_Easy/0168_Excel_Sheet_Column_Title.py
+++ 01_Easy/0168_Excel_Sheet_Column_Title.py
@@ -5,9 +5,11 @@ class Solution:
         while columnNumber > 0:
             columnNumber -= 1  # Shift to account not having a 0 in the number system
             num_letter = columnNumber % 26
-            letter_list.insert(0, chr(num_letter+65))  # 0->A; 1->B;...
+            letter_list.append(chr(num_letter+65))  # 0->A; 1->B;...
 
             columnNumber = columnNumber // 26
         
+        # Reverse the list to get the correct order and join to form the final string
+        letter_list.reverse()
         return ''.join(letter_list)
 
